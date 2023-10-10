dc7e780be08ad40f5f1348aef49a7b7a1fe27caa
Thomas Bikias <thomas.bikias@bsse.ethz.ch>
2023-10-10 14:55:31 +0200

    implemented extract embedding functions for progen models

--- models/model_sanbox.py
+++ models/model_sanbox.py
@@ -1,9 +1,11 @@
 from models.pretrained_models import *
 from models.models import LinearRegression
 
-model = ProgenSmall151M()
+model = ProGenPLM()
 
-head = LinearRegression(1024 , 1)
-#model.concat_task_specific_head(head)
+head = LinearRegression(32 , 1)
+model.concat_task_specific_head(head)
 
-model.fine_tune('aav' , 'full_retrain' , epochs = 5 , lr = 0.001 , optimizer = 'adam' , batch_size = 16 , train_split_name = 'two_vs_many_split', val_split = 0.2 , loss_f = 'mse' )
+#model.fine_tune('aav' , 'full_retrain' , epochs = 5 , lr = 0.001 , optimizer = 'adam' , batch_size = 2, train_split_name = 'two_vs_many_split', val_split = 0.2 , loss_f = 'mse' )
+
+model.extract_embeddings('aav' , batch_size = 1 , layer = 12 )
--- models/pretrained_models.py
+++ models/pretrained_models.py
@@ -6,11 +6,11 @@ import torch
 import torch.utils.data as data_utils
 from torch.utils.data import DataLoader
 import time
+from abc import abstractmethod
 
-class PretrainedProteinLanguageModel():
+class IPretrainedProteinLanguageModel():
 
-    py_model = None
-       
+    py_model = None     
     tokenizer = None
     name = ""
     fine_tuned = 'none'
@@ -18,12 +18,12 @@ class PretrainedProteinLanguageModel():
     head = 'none'
     no_parameters = 0
     no_layers = 0
+    emb_layers_dim = 0
+    output_dim = 0
     
-    def __init__(self, name, py_model , tokenizer):
-        self.name = name
-        self.py_model = py_model
-        self.tokenizer = tokenizer
-
+    def __init__(self):
+        pass
+        
     def get_name(self):
         return self.name
 
@@ -41,30 +41,39 @@ class PretrainedProteinLanguageModel():
 
     def set_tokenizer(self, tokenizer):
         self.tokenizer = tokenizer
-
-    def fine_tune(self, data_type , method):
+        
+    @abstractmethod    
+    def concat_task_specific_head(self):
         pass
-    
+    @abstractmethod
+    def extract_embeddings(self, layer):
+        pass
+    @abstractmethod
+    def fine_tune(self, data_type , method):
+        pass 
+    @abstractmethod
     def evaluate(self, data_type ):
         pass
         
-    def concat_task_specific_head(self):
-        pass
     
-    def extract_embeddings(self, layer):
-        pass
     
     
         
-class ProgenSmall151M(PretrainedProteinLanguageModel):
-    def __init__(self):
-        PretrainedProteinLanguageModel.__init__(self, 'progen2-small', ProGenForCausalLM.from_pretrained('./language_models/progen2/checkpoints/progen2-small') , utils.load_tokenizer('progen2-small'))
-        
-        
-    def concat_task_specific_head(self , head):
-    
-       # assert head.in_.in_features == embs.shape[1], f'Embeddings dimension ({embs.shape[1]}) is not compatible with the input size of the task specific head ({head.in_.in_features}) . Change "input_len" to {embs.shape[1]} in config file : {args.head_config}'
-     
+class ProGenPLM(IPretrainedProteinLanguageModel):
+    def __init__(self , progen_model_name = 'progen2-small'):
+        #IPretrainedProteinLanguageModel.__init__(self)
+        self.name = progen_model_name
+        self.py_model = ProGenForCausalLM.from_pretrained(f'./language_models/progen2/checkpoints/{progen_model_name}')
+        self.tokenizer = utils.load_tokenizer(progen_model_name)
+        self.no_parameters = utils.get_parameters(self.py_model)
+        self.no_layers = len(self.py_model.transformer.h)
+        self.output_dim = self.py_model.lm_head.out_features
+        self.emb_layers_dim = self.py_model.transformer.h[0].attn.out_proj.out_features
+            
+      
+    def concat_task_specific_head(self , head):  
+        assert head.in_.in_features == self.output_dim, f' Head\'s input dimension ({head.in_.in_features}) is not compatible with {self.name}\'s output dimension ({self.output_dim}). To concat modules these must be equal.'
+        #TODO: Add concat option with lm head or final transformer layer.
         self.py_model = nn.Sequential(
           self.py_model,
           head
@@ -72,19 +81,70 @@ class ProgenSmall151M(PretrainedProteinLanguageModel):
         self.head = head.__class__.__name__ ## parse name from variable
         return 
         
-    def extract_embeddings(self , data_type , layer = 11, reduction = 'mean'):
-        logger = l.Logger(f'extract_embeddings_{self.name}_{self.head}_{data_type}_layer:{layer}_reduction:{reduction}.txt')
-        logger.log('Extracting embeddings...')
-        pass
+    def extract_embeddings(self , data_type , batch_size , layer = 11, reduction = 'mean'):
+        logger = l.Logger(f'[logger]extract_embeddings_{data_type}_{self.name}_layer{layer}_{reduction}.txt')
+        device = 'cpu'
+        fp16 = False
+        device_ids = []
+        if torch.cuda.is_available():
+            device = "cuda:0"
+            fp16 = True
+            logger.log(f'Available GPUs : {torch.cuda.device_count()}')
+            for i in range(torch.cuda.device_count()):
+                logger.log(f' Running on {torch.cuda.get_device_properties(i).name}')
+                device_ids.append(i)
+
+        else:
+            logger.log(f' No gpu found rolling device back to {device}')
+        data = utils.load_dataset(data_type)  
+        start_enc_time = time.time()
+        logger.log(f' Encoding {data.shape[0]} sequences....')
+        encs = utils.categorical_encode(data['aa_seq'].values, self.tokenizer , max(data['len'].values))   
+        logger.log(f' Encoding completed! {time.time() -  start_enc_time:.4f}s')
+        seq_dataset = data_utils.TensorDataset(encs)
+        seq_loader =  data_utils.DataLoader(seq_dataset, batch_size= batch_size, shuffle=False)
+        logger.log(f'Extracting embeddings for {len(seq_dataset)} sequences...')
+        
+        embs = torch.zeros((len(seq_dataset), self.emb_layers_dim)).to(device) ### FIX: Find embeddings dimension either hard coded for model or real the pytorch model of ProGen. Maybe add reduction dimension as well
+        self.py_model = self.py_model.to(device)
+        i = 0
+        self.py_model.eval()
+        with torch.no_grad():
+            with torch.cuda.amp.autocast(enabled= fp16):
+                for batch in seq_loader:
+                    start = time.time()
+                    if layer == 13:
+                        out = self.py_model(batch[0]).logits
+                    else:
+                        out = self.py_model(batch[0]).hidden_states
+                        out = out[layer - 1] 
+                   
+                    if reduction == 'mean':
+                        embs[i : i+ batch_size, : ] = torch.mean(out , dim = 1)
+                    elif reduction == 'sum':
+                        embs[i : i+ batch_size, : ] = torch.sum(out , dim = 1)
+                    else:
+                        raise 'Unsupported reduction option'
+                    del out
+                    i = i + batch_size
+                    logger.log(f' {i} / {len(seq_dataset)} | {time.time() - start:.2f}s ') # | memory usage : {100 - memory_usage.percent:.2f}%
+           
+
+
+        torch.save(embs,f'./data/{data_type}/embeddings/{data_type}_{self.name}_embs_layer{layer}_{reduction}.pt')
+        t = torch.load(f'./data/{data_type}/embeddings/{data_type}_{self.name}_embs_layer{layer}_{reduction}.pt')
+        logger.log(f'Saved embeddings ({t.shape[1]}-d) as "{data_type}_{self.name}_embs_layer{layer}_{reduction}.pt" ({time.time() - start_enc_time:.2f}s)')
+        return
     
     def fine_tune(self, data_type , method , epochs , lr , optimizer , batch_size , train_split_name , val_split , loss_f , lr_scheduler = None , log_interval = 1000 ):
-        
-        logger = l.Logger(f'fine_tune_{self.name}_{self.head}_{method}_{data_type}.txt')
+        logger = l.Logger(f'[logger]fine_tune_{self.name}_{self.head}_{method}_{data_type}.txt')
         
         data = utils.load_dataset(data_type)   
         
         logger.log(f' Encoding {data.shape[0]} sequences....')
+        start_enc_time = time.time()
         encs = utils.categorical_encode(data['aa_seq'].values, self.tokenizer , max(data['len'].values))   
+        logger.log(f' Encoding completed! {time.time() -  start_enc_time:.4f}s')
         data_train = data[data[train_split_name] == 'train']
         data_test = data[data[train_split_name] == 'test']
         encs_train = encs[data_train.index]
@@ -98,7 +158,7 @@ class ProgenSmall151M(PretrainedProteinLanguageModel):
         train_dataloader = DataLoader(train_set, batch_size = batch_size , shuffle=True)
         valid_dataloader = DataLoader(val_set, batch_size = batch_size , shuffle=True)
         test_dataloader = DataLoader(test_dataset, batch_size = batch_size, shuffle=True)
-
+    
         
         if optimizer == 'adam':
             optimizer = torch.optim.Adam(self.py_model.parameters(), lr=lr , betas=(0.9, 0.95))
@@ -120,7 +180,7 @@ class ProgenSmall151M(PretrainedProteinLanguageModel):
         epoch_train_loss = []
         epoch_val_loss = []
         
-        trainig_start_time = time.time()
+        training_start_time = time.time()
         
         for epoch in range(epochs):
             
@@ -141,7 +201,7 @@ class ProgenSmall151M(PretrainedProteinLanguageModel):
                 for itr , trainig_data in enumerate(dataloader , 0):
                     optimizer.zero_grad()
                     training_batch , training_labels = trainig_data
-                    logger.log(f' {training_batch[0]=}')
+                    logger.log(f' {training_batch.shape=}')
                     logger.log(f' {training_labels=}')
                     outputs = self.py_model(training_batch)  
                     loss = loss_f(torch.squeeze(outputs).float(), training_labels.float())  
@@ -160,3 +220,13 @@ class ProgenSmall151M(PretrainedProteinLanguageModel):
                     epoch_val_loss.append(epoch_loss)
                 
                 logger.log('({}) Loss: {:.4f} {:.4f}s'.format(phase, epoch_loss, time.now() - epoch_start_time))
+        self.fine_tuned = method     
+        logger.log(' Finetuning  ({}) on {} data completed after {:.4f}s '.format(method , data_type , time.now() - training_start_time))
+        
+        ### Add save finetuned model + saved best model based on early stopping
+        return
+
+    def evaluate(self):
+        return 0
+    
+
