0abf600ba3dc248287d61a41b75407c7220eb977
Thomas Bikias <thomas.bikias@bsse.ethz.ch>
2023-10-10 18:08:04 +0200

    removed unnessecary files

--- ft_frameworks.py
+++
@@ -1,1 +0,0 @@
-import torch.nn as nnimport torchdef feature_extraction(dataset_dict, head, lr , epochs, optimizer = 'Adam', criterion = nn.MSELoss(), lr_scheduler = False ):        ft_model = head    optimizer = torch.optim.Adam(ft_model.parameters(), lr=lr , betas=(0.9, 0.95))          return headdef train():    return 0
