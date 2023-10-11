980a5bfefe833f2939776ee16e3f8b952e53c372
Thomas Bikias <tbikias@eu-login-17.euler.ethz.ch>
2023-10-11 18:23:38 +0200

    saved logger to loggers folder

--- logger.py
+++ logger.py
@@ -4,13 +4,13 @@ import os
 class Logger():
     file_name = ''
     created_at = None
-
+    location = './loggers'
     def __init__(self, file_name: str):
         
         self.created_at = datetime.datetime.now()
         self.file_name = f'{file_name}_{self.created_at}'
         #file_name = f'{file_name}_{self.created_at}'
-        with open(file_name, 'w') as f:
+        with open(f'{self.location}/{self.file_name}', 'w') as f:
             f.truncate(0)
             f.close()
         
@@ -18,7 +18,7 @@ class Logger():
             
             
     def log(self, text: str):
-        with open(self.file_name, 'a') as f: 
+        with open(f'{self.location}/{self.file_name}', 'a') as f: 
             f.write(text)
             f.write('\n')
             f.close()
