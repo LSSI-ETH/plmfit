abfd9fa163e7c42b11e152a91055cb6606104ff0
Thomas Bikias <thomas.bikias@bsse.ethz.ch>
2023-10-26 11:57:06 +0200

    call tuner.train for fine_tune function

--- models/pretrained_models.py
+++ models/pretrained_models.py
@@ -8,22 +8,23 @@ from torch.utils.data import DataLoader
 import time
 from abc import abstractmethod
 from models.fine_tuning import *
+from tokenizers import Tokenizer
 
-class IPretrainedProteinLanguageModel():
-
-    py_model = None     
-    tokenizer = None
-    name = ""
-    fine_tuned = 'none'
-    data_type = 'none'
-    head = 'none'
-    no_parameters = 0
-    no_layers = 0
-    emb_layers_dim = 0
-    output_dim = 0
-    tuner = None
+class IPretrainedProteinLanguageModel(nn.Module):
     
-    def __init__(self , progen_model_name = 'progen2-small'):
+    name : str
+    py_model : nn.Module
+    head : nn.Module
+    head_name : str
+    no_parameters : int
+    emb_layers_dim : int
+    output_dim : int
+    
+    
+    def __init__(self):
+        super().__init__()
+        self.head = None
+        self.head_name = 'none'
         pass
         
     def get_name(self):
@@ -45,7 +46,7 @@ class IPretrainedProteinLanguageModel():
         self.tokenizer = tokenizer
         
     @abstractmethod    
-    def concat_task_specific_head(self):
+    def concat_task_specific_head(self , head):
         pass
     @abstractmethod
     def extract_embeddings(self, layer):
@@ -55,6 +56,10 @@ class IPretrainedProteinLanguageModel():
         pass 
     @abstractmethod
     def evaluate(self, data_type ):
+        pass
+    
+    @abstractmethod
+    def forward(self, src):
         pass
 
 #TODO: infere based on aa_seq list
@@ -67,19 +72,19 @@ class IPretrainedProteinLanguageModel():
   #Implement class for every supported Portein Language Model family  
         
 class ProGenPLM(IPretrainedProteinLanguageModel): ##
-    def __init__(self , progen_model_name : str, tuner : Tuner):
+
+    tokenizer : Tokenizer
+    
+    def __init__(self , progen_model_name : str):
         #IPretrainedProteinLanguageModel.__init__(self)
         super().__init__()
-        self.name = progen_model_name
-        self.py_model = ProGenForCausalLM.from_pretrained(f'./language_models/progen2/checkpoints/{progen_model_name}')
-        self.tokenizer = utils.load_tokenizer(progen_model_name)
+        self.name = progen_model_name 
+        self.py_model = ProGenForCausalLM.from_pretrained(f'./language_models/progen2/checkpoints/{progen_model_name}')    
         self.no_parameters = utils.get_parameters(self.py_model)
         self.no_layers = len(self.py_model.transformer.h)
         self.output_dim = self.py_model.lm_head.out_features
         self.emb_layers_dim = self.py_model.transformer.h[0].attn.out_proj.out_features
-        self.head = None
-        self.head_name = 'none'
-        self.tuner = tuner 
+        self.tokenizer = utils.load_tokenizer(progen_model_name)
       
     def concat_task_specific_head(self , head):  
         assert head.in_.in_features == self.output_dim, f' Head\'s input dimension ({head.in_.in_features}) is not compatible with {self.name}\'s output dimension ({self.output_dim}). To concat modules these must be equal.'
@@ -145,31 +150,11 @@ class ProGenPLM(IPretrainedProteinLanguageModel): ##
         logger.log(f'Saved embeddings ({t.shape[1]}-d) as "{data_type}_{self.name}_embs_layer{layer}_{reduction}.pt" ({time.time() - start_enc_time:.2f}s)')
         return
     
-    def fine_tune(self, data_type , method , epochs , lr , optimizer , batch_size , train_split_name , val_split , loss_f , lr_scheduler = None , log_interval = 1000 ):
+    def fine_tune(self, data_type, tuner , train_split_name, optimizer , loss_f ):
         
         assert self.head != None , 'Task specific head haven\'t specified.'
-        logger = l.Logger(f'logger_fine_tune_{self.name}_{self.head_name}_{method}_{data_type}.txt')
-        device = 'cpu'
-        fp16 = False
-        device_ids = []
-        if torch.cuda.is_available():
-            device = "cuda:0"
-            fp16 = True
-            logger.log(f'Available GPUs : {torch.cuda.device_count()}')
-            for i in range(torch.cuda.device_count()):
-                logger.log(f' Running on {torch.cuda.get_device_properties(i).name}')
-                device_ids.append(i)
-
-        else:
-            logger.log(f' No gpu found rolling device back to {device}')
-
-        
-        
-        self.py_model = self.py_model.to(device)
-        self.head = self.head.to(device)
-        
-        data = utils.load_dataset(data_type)   
-        
+        logger = l.Logger(f'logger_fine_tune_{self.name}_{self.head_name}_{tuner.method}_{data_type}.txt')
+        data = utils.load_dataset(data_type)           
         logger.log(f' Encoding {data.shape[0]} sequences....')
         start_enc_time = time.time()
         encs = utils.categorical_encode(data['aa_seq'].values, self.tokenizer , max(data['len'].values))   
@@ -179,83 +164,23 @@ class ProGenPLM(IPretrainedProteinLanguageModel): ##
         encs_train = encs[data_train.index]
         encs_test = encs[data_test.index]
         train_dataset = data_utils.TensorDataset( encs_train , torch.tensor(data_train['score'].values))  
-        n_val_samples = int(val_split * len(train_dataset))
+        n_val_samples = int(tuner.val_split * len(train_dataset))
         n_train_samples = len(train_dataset) - n_val_samples 
         train_set, val_set = torch.utils.data.random_split(train_dataset , [n_train_samples, n_val_samples]) 
-        test_dataset = data_utils.TensorDataset( encs_test  , torch.tensor(data_test['score'].values))     
+        test_dataset = data_utils.TensorDataset( encs_test  , torch.tensor(data_test['score'].values))             
+        train_dataloader = DataLoader(train_set, batch_size = tuner.batch_size , shuffle=True)
+        valid_dataloader = DataLoader(val_set, batch_size = tuner.batch_size , shuffle=True)
+        test_dataloader = DataLoader(test_dataset, batch_size = tuner.batch_size, shuffle=True)
         
-        train_dataloader = DataLoader(train_set, batch_size = batch_size , shuffle=True)
-        valid_dataloader = DataLoader(val_set, batch_size = batch_size , shuffle=True)
-        test_dataloader = DataLoader(test_dataset, batch_size = batch_size, shuffle=True)
+        dataloader_dict = { 'train' : train_dataloader , 'val' : valid_dataloader}
     
-        
-        if optimizer == 'adam':
-            optimizer = torch.optim.Adam(self.py_model.parameters(), lr=lr , betas=(0.9, 0.99), weight_decay= 0.1)
-            #torch.nn.utils.clip_grad_norm_(self.py_model.parameters(), 1 , norm_type=2.0, error_if_nonfinite=False)
-        else:
-            raise 'Unsupported optimizer'
-            
-        if  loss_f == 'mse':
-            loss_f = nn.MSELoss(reduction = 'mean')
-        else:
-            raise 'Unsupported criterion'
-            
-        
-        self.tuner.set_trainable_parameters(self)
+        tuner.set_trainable_parameters(self)
         ## Check if parameters of self model are affected just by calling them as argument
             ##TODO: move the whole training loop in tuner method train
-        epoch_train_loss = []
-        epoch_val_loss = []
-        
         training_start_time = time.time()
-        
-        for epoch in range(epochs):
-            
-            epoch_start_time = time.time()
-            logger.log('Epoch {}/{}'.format(epoch + 1, epochs))
-            logger.log('-' * 10)
-            for phase in ['train', 'val']:
-                
-                if phase == 'train':
-                    self.py_model.train()  # Set model to training mode
-                    self.head.train()
-                    dataloader = train_dataloader
-                else:
-                    self.py_model.eval()   # Set model to evaluate mode
-                    self.head.eval()
-                    dataloader = valid_dataloader
-                    
-                batch_loss = 0
-                    
-                for itr , trainig_data in enumerate(dataloader , 0):
-                    batch_start_time = time.time()
-                    optimizer.zero_grad()
-                    training_batch , training_labels = trainig_data
-                    training_batch = training_batch.to(device)
-                    training_labels = training_labels.to(device)
-                    outputs = self.forward(training_batch)
-                    logger.log(f'{torch.squeeze(outputs).float()}')
-                    logger.log(f'{training_labels.float()}')
-                    loss = loss_f(torch.squeeze(outputs).float(), training_labels.float())  
-                    if phase == 'train':
-                           loss.backward()
-                           optimizer.step()
-                    batch_loss += loss.item()       
-                    
-                    if itr%log_interval == 0:
-                        logger.log(f'({phase}) batch : {itr + 1}  / {len(dataloader)} | running_loss : {batch_loss / (itr + 1)} (batch time : {time.time() - batch_start_time:.4f})')
-                
-                epoch_loss = batch_loss /itr
-                if phase == 'train':
-                    epoch_train_loss.append(epoch_loss)
-                else:
-                    epoch_val_loss.append(epoch_loss)
-                
-                logger.log('({}) Loss: {:.4f} {:.4f}s'.format(phase, epoch_loss, time.now() - epoch_start_time))
-        self.fine_tuned = method     
-        logger.log(' Finetuning  ({}) on {} data completed after {:.4f}s '.format(method , data_type , time.now() - training_start_time))
-        
-        ### Add save finetuned model + saved best model based on early stopping
+        tuner.train(self, dataloader_dict , optimizer , loss_f , logger)
+        logger.log(' Finetuning  ({}) on {} data completed after {:.4f}s '.format(tuner.method , data_type , time.time() - training_start_time))
+        self.fine_tuned = tuner.method
         return
 
     def evaluate(self):
