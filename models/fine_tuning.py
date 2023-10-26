30c5c00092eb5e0bad20d0ea214fb975259e7271
Thomas Bikias <thomas.bikias@bsse.ethz.ch>
2023-10-26 12:21:02 +0200

    save best model

--- models/fine_tuning.py
+++ models/fine_tuning.py
@@ -5,6 +5,7 @@ import utils
 import torch.nn as nn
 import torch
 import time
+import os
 
 class Tuner():
 
@@ -17,19 +18,13 @@ class Tuner():
     epochs = 0
     
     def __init__(self, method, epochs, lr , batch_size,  val_split, log_interval):
-        print('init')
         self.method = method
         self.lr = lr
         self.batch_size = batch_size
-
         self.val_split = val_split
         self.log_interval = log_interval
         self.epochs = epochs
-                
-
-            
-        
-        
+                    
     @abstractmethod    
     def set_trainable_parameters(self, model):
         pass
@@ -48,11 +43,11 @@ class FullRetrainTuner(Tuner):
         utils.get_parameters(model.py_model , True)
         utils.get_parameters(model.head , True)
 
-    def train(self, model , dataloaders_dict, optimizer, loss_f , logger):
-        
+    def train(self, model , dataloaders_dict, optimizer, loss_f , logger):      
         device = 'cpu'
         fp16 = False
         device_ids = []
+        ##TODO: Run on multiple GPUs data and model parallelization
         if torch.cuda.is_available():
             device = "cuda:0"
             fp16 = True
@@ -60,7 +55,6 @@ class FullRetrainTuner(Tuner):
             for i in range(torch.cuda.device_count()):
                 logger.log(f' Running on {torch.cuda.get_device_properties(i).name}')
                 device_ids.append(i)
-
         else:
             logger.log(f' No gpu found rolling device back to {device}')
             
@@ -81,6 +75,8 @@ class FullRetrainTuner(Tuner):
             
         epoch_train_loss = []
         epoch_val_loss = []
+        best_val_loss = float('inf')
+        best_epoch = 0
         
         for epoch in range(self.epochs):
             
@@ -116,14 +112,17 @@ class FullRetrainTuner(Tuner):
                     if itr%self.log_interval == 0:
                         logger.log(f'({phase}) batch : {itr + 1}  / {len(dataloaders_dict[phase])} | running_loss : {batch_loss / (itr + 1)} (batch time : {time.time() - batch_start_time:.4f})')
                 
-                epoch_loss = batch_loss /itr
+                epoch_loss = batch_loss / itr
                 if phase == 'train':
                     epoch_train_loss.append(epoch_loss)
                 else:
                     epoch_val_loss.append(epoch_loss)
                 
                 logger.log('({}) Loss: {:.4f} {:.4f}s'.format(phase, epoch_loss, time.now() - epoch_start_time))
-  
-
-        
-        ### Add save finetuned model + saved best model based on early stopping
+                
+            ##TODO: Implement early stopping
+            if epoch_val_loss[-1] < best_val_loss:
+                best_val_loss = epoch_val_loss[-1] 
+                torch.save(model.state_dict(), f'./models/saved_models/model:{model.name}_head:{model.head_name}_ft:{self.method}.pt')
+                best_epoch  = epoch
+          
