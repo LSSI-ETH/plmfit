b6f7ce19be5a847988f9797665f81d5370652ad7
Thomas Bikias <thomas.bikias@bsse.ethz.ch>
2023-11-03 10:32:47 +0100

    tested concatination

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     
     #fine_tuner = FullRetrainFineTuner(epochs = 5 , lr = 0.0006, batch_size = 8,  val_split = 0.2 , log_interval = 1)
     #pro_model = ProGenFamily(progen_model_name = 'progen2-small')
-   # head = LinearRegression(32 , 1)
+   # 
     #model = ESMFamily(esm_model_name = 'esm_small')
     #configuration = EsmConfig()
     #model = EsmModel(configuration)
@@ -25,7 +25,9 @@ if __name__ == '__main__':
 
     #model.extract_embeddings('aav' , batch_size = args.batch_size , layer = args.layer )
     seq = 'ABCDEFG'
-    esm_version  = 'esm2_t6_8M_UR50D'
+    esm_version  = 'esm2_t30_150M_UR50D'
     model = ESMFamily(esm_version)
+    head = LinearRegression(640 , 1)
+    model.concat_task_specific_head(head)
+    fine_tuner = FullRetrainFineTuner(epochs = 5 , lr = 0.0006, batch_size = 8,  val_split = 0.2 , log_interval = 1)
     print(model.no_parameters)
-    print(utils.get_parameters(model))
