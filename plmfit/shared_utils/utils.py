b8a4d8d3fe43758e994bcfc383da68287bbcdd24
championsnet <67751397+championsnet@users.noreply.github.com>
2024-02-05 11:32:01 +0100

    Updated absolute paths

--- plmfit/shared_utils/utils.py
+++ plmfit/shared_utils/utils.py
@@ -7,20 +7,20 @@ from transformers import AutoTokenizer
 
 
 def load_model(model_name):
-    return ProGenForCausalLM.from_pretrained(f'./language_models/progen2/checkpoints/{model_name}')
+    return ProGenForCausalLM.from_pretrained(f'./plmfit/language_models/progen2/checkpoints/{model_name}')
 
 
 def load_embeddings(data_type, embs):
-    embs_file = f'./data/{data_type}/embeddings/{embs}'
+    embs_file = f'./plmfit/data/{data_type}/embeddings/{embs}'
     return torch.load(f'{embs_file}.pt', map_location=torch.device('cpu'))
 
 
 def load_dataset(data_type):
-    return pd.read_csv(f'./data/{data_type}/{data_type}_data_full.csv')
+    return pd.read_csv(f'./plmfit/data/{data_type}/{data_type}_data_full.csv')
 
 
 def get_wild_type(data_type):
-    file = f'./data/{data_type}'
+    file = f'./plmfit/data/{data_type}'
     wild_type_f = open(f'{file}/wild_type.json')
     wt = json.load(wild_type_f)['wild_type']
     return wt
@@ -30,14 +30,14 @@ def load_tokenizer(model_name):
     model_file = ''
     if 'progen2' in model_name:
         model_file = 'progen2'
-    file = f'./language_models/{model_file}/tokenizer.json'
+    file = f'./plmfit/language_models/{model_file}/tokenizer.json'
 
     with open(file, 'r') as f:
         return Tokenizer.from_str(f.read())
 
 
 def load_head_config(config_file):
-    file = f'./models/{config_file}'
+    file = f'./plmfit/models/{config_file}'
     config_f = open(f'{file}.json')
     config = json.load(config_f)
     print(config)
