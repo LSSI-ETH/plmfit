0bd2abbef803c067088c88a34c759c82113fae7e
championsnet <vaggelisstam@gmail.com>
2024-02-06 12:30:47 +0100

    Inter-compatible package versions

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from plmfit.models.pretrained_models import *
 from plmfit.models.downstream_heads import LinearRegression
 from plmfit.models.fine_tuning import *
 import argparse
+import plmfit.logger as l
 
 parser = argparse.ArgumentParser(
     description='Embeddings extraction', fromfile_prefix_chars='@')
@@ -10,12 +11,13 @@ parser.add_argument('--batch_size', type=int, default=1)
 args = parser.parse_args()
 
 if __name__ == '__main__':
-
+    logger = l.Logger("./train_logger")
     fine_tuner = FullRetrainFineTuner(
         epochs=5, lr=0.0006, batch_size=8,  val_split=0.2, log_interval=1)
     model = ProGenFamily(progen_model_name='progen2-small')
-    head = LinearRegression(32, 1)
-    model.concat_task_specific_head(head)
-    model.fine_tune('aav', fine_tuner, 'two_vs_many_split', 'adam', 'mse')
+    logger.log('Model loaded')
+    # head = LinearRegression(32, 1)
+    # model.concat_task_specific_head(head)
+    # model.fine_tune('meltome', fine_tuner, 'set', 'adam', 'mse')
 
-    # model.extract_embeddings('aav' , batch_size = args.batch_size , layer = args.layer )
+    model.extract_embeddings('gb1' , batch_size = args.batch_size , layer = args.layer )
