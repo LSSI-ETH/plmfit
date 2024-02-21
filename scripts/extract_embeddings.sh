9843360fbc7b49d00019c4aa8caf2d4bcf971a81
championsnet <vaggelisstam@gmail.com>
2024-02-21 16:54:43 +0100

    Lessened required resources

--- scripts/extract_embeddings.sh
+++ scripts/extract_embeddings.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=extract_embeddings    # create a short name for your job
 #SBATCH --nodes=1            # node count
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=12g      # total memory per node (8 GB per cpu-core is default)
+#SBATCH --mem-per-cpu=8g      # total memory per node (8 GB per cpu-core is default)
 #SBATCH --cpus-per-task=1
 #SBATCH --tasks-per-node=1
 #SBATCH --gpus=1 
-#SBATCH --gres=gpumem:20g        #gpu memory
+#SBATCH --gres=gpumem:16g        #gpu memory
 #SBATCH --gpus-per-node=1
 #SBATCH --time=12:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --output=experiments/config_%j/out.out
@@ -14,4 +14,4 @@
 
 module load gcc/8.2.0  python_gpu/3.11.2
 
-python3 plmfit.py --function extract_embeddings --layer last --reduction bos --data_type gb1 --plm progen2-medium --output_dir $SCRATCH
+python3 plmfit.py --function extract_embeddings --layer last --reduction 560 --data_type aav --plm progen2-medium --output_dir $SCRATCH
--- scripts/extract_embeddings_xlarge.sh
+++ scripts/extract_embeddings_xlarge.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=extract_embeddings    # create a short name for your job
 #SBATCH --nodes=1            # node count
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=100g      # total memory per node (8 GB per cpu-core is default)
+#SBATCH --mem-per-cpu=40g      # total memory per node (8 GB per cpu-core is default)
 #SBATCH --cpus-per-task=1
 #SBATCH --tasks-per-node=1
 #SBATCH --gpus=1 
@@ -14,4 +14,4 @@
 
 module load gcc/8.2.0  python_gpu/3.11.2
 
-python3 plmfit.py --function extract_embeddings --layer last --reduction bos --data_type gb1 --plm progen2-xlarge --output_dir $SCRATCH
+python3 plmfit.py --function extract_embeddings --layer first --reduction mean --data_type aav --plm progen2-xlarge --output_dir $SCRATCH
