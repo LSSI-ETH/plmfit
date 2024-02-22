d11e48f646bd92c09fa0ef1760ab34179385469a
bikias.thomas@gmail.com <bikias.thomas@gmai.com>
2024-02-22 12:16:32 +0100

    Created load embeddings method + minor changes

--- plmfit/shared_utils/utils.py
+++ plmfit/shared_utils/utils.py
@@ -1,11 +1,12 @@
-#from plmfit.language_models.progen2.models.progen.modeling_progen import ProGenForCausalLM
+from sklearn.preprocessing import StandardScaler
 import torch
 import json
 import pandas as pd
 from tokenizers import Tokenizer
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
 
-
-#def load_model(model_name):
+# def load_model(model_name):
 #   return ProGenForCausalLM.from_pretrained(f'./plmfit/language_models/progen2/checkpoints/{model_name}')
 
 
@@ -16,6 +17,114 @@ def load_embeddings(data_type, embs):
 
 def load_dataset(data_type):
     return pd.read_csv(f'./plmfit/data/{data_type}/{data_type}_data_full.csv')
+
+
+def load_embeddings(emb_path=None, data_type='aav', layer='last', model='progen2-small', reduction='mean', device='cpu'):
+    """
+    Process data based on either a provided data path or specified data type, layer, model, and reduction method.
+
+    Parameters:
+        emb_path (str): Path to the embeddings file. If provided, other arguments are ignored (except for storage device).
+        data_type (str): Type of data (default is 'aav').
+        layer (str): Layer information (default is 'last').
+        model (str): Model information (default is 'progen2-small').
+        reduction (str): Reduction method (default is 'mean').
+    """
+    if emb_path is None:
+        # Process data using the provided data path
+        emb_path = f'./plmfit/data/{data_type}/embeddings/{data_type}_{model}_embs_layer{layer}_{reduction}.pt'
+
+    embeddings = torch.load(emb_path, map_location=torch.device(device))
+    embeddings = embeddings.numpy() if embeddings.is_cuda else embeddings
+    return torch.tensor(embeddings, dtype=torch.float32)
+
+
+def create_data_loaders(dataset, scores, split=None, test_size=0.2, validation_size=0.1, batch_size=64, scaler=None):
+    """
+    Create DataLoader objects for training, validation, and testing.
+
+    Parameters:
+        dataset (numpy.ndarray): Input dataset.
+        scores (numpy.ndarray): Scores aligned with dataset.
+        split (numpy.ndarray): Array indicating the split for each sample (train, test, validation).
+                                If provided, test_size and validation_size are ignored.
+        test_size (float): Fraction of the data to be used as the test set (default is 0.2).
+        validation_size (float): Fraction of the training data to be used as the validation set (default is 0.1).
+        batch_size (int): Batch size for DataLoader (default is 64).
+        scaler (str): Scaler name for feature scaling (default is None). Supported scalers: 'standard'.
+
+    Returns:
+        dict: Dictionary containing DataLoader objects for train, validation, and test.
+    """
+    if split is None:
+        # Split data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(
+            dataset, scores, test_size=test_size, random_state=42)
+
+        # Further split the training data into training and validation sets
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train, y_train, test_size=validation_size/(1-test_size), random_state=42)
+
+    # TODO: see if this works for provided sets
+    else:
+        # Use the provided split
+        X_train = dataset[split == 'train']
+        X_test = dataset[split == 'test']
+        X_val = dataset[split == 'validation']
+        y_train = scores[split == 'train']
+        y_test = scores[split == 'test']
+        y_val = scores[split == 'validation']
+
+    # Scale the features if scaler is provided
+    if scaler is not None:
+        if scaler == 'standard':
+            scaler = StandardScaler()
+            X_train = scaler.fit_transform(X_train)
+            X_val = scaler.transform(X_val)
+            X_test = scaler.transform(X_test)
+        else:
+            raise "Unsupported scaler. Use 'standard' or None."
+
+    # Convert splits to PyTorch tensors
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    X_val = torch.tensor(X_val, dtype=torch.float32)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.float32)
+    y_val = torch.tensor(y_val, dtype=torch.float32)
+    y_test = torch.tensor(y_test, dtype=torch.float32)
+
+    # Create DataLoader for training, validation, and testing
+    train_dataset = TensorDataset(X_train, y_train)
+    val_dataset = TensorDataset(X_val, y_val)
+    test_dataset = TensorDataset(X_test, y_test)
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False)
+
+    return {'train': train_loader, 'val': val_loader, 'test': test_loader}
+
+
+def load_head_config(config_file_name):
+    """
+    Load a head configuration file from the plmfit/models/configurations directory.
+
+    Parameters:
+        config_file_name (str): Name of the head configuration file.
+
+    Returns:
+        dict: Loaded configuration.
+    """
+    # Construct the full path to the configuration file
+    config_file_path = f'./plmfit/models/configurations/{config_file_name}'
+
+    # Load the configuration from the file
+    with open(config_file_path, 'r') as file:
+        config = json.load(file)
+
+    return config
 
 
 def get_wild_type(data_type):
@@ -33,14 +142,6 @@ def load_tokenizer(model_name):
 
     with open(file, 'r') as f:
         return Tokenizer.from_str(f.read())
-
-
-def load_head_config(config_file):
-    file = f'./plmfit/models/{config_file}'
-    config_f = open(f'{file}.json')
-    config = json.load(config_f)
-    print(config)
-    return config
 
 
 def one_hot_encode(seqs):
