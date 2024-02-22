2d5b2b8a6734623eba4699443ff88626f8277ad3
bikias.thomas@gmail.com <bikias.thomas@gmai.com>
2024-02-22 12:17:14 +0100

    Added classification and regression evaluation

--- plmfit/shared_utils/data_explore.py
+++ plmfit/shared_utils/data_explore.py
@@ -5,12 +5,17 @@ import numpy as np
 import plmfit.shared_utils.utils as utils
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
+from sklearn.metrics import roc_curve, auc, roc_auc_score, confusion_matrix, matthews_corrcoef
 import torch
+from sklearn.metrics import matthews_corrcoef, confusion_matrix, roc_auc_score, mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
+
 
 def plot_label_distribution(data, label="binary_score", path=None, text="Keep"):
     sns.set(style="whitegrid")
     plt.figure(figsize=(10, 6))
-    ax = sns.countplot(x=label, data=data, hue=label, palette=["coral", "skyblue"])
+    ax = sns.countplot(x=label, data=data, hue=label,
+                       palette=["coral", "skyblue"])
     plt.title('Label Distribution', fontsize=16)
     plt.xlabel(text + ' Label', fontsize=14)
     plt.ylabel('Count', fontsize=14)
@@ -29,11 +34,11 @@ def plot_label_distribution(data, label="binary_score", path=None, text="Keep"):
         plt.show()
 
 
-
 def plot_score_distribution(data, column="score", text="Fitness Score", log_scale=False, path=None):
     sns.set(style="whitegrid")
     plt.figure(figsize=(10, 6))
-    sns.histplot(data[column], bins=1000, kde=True, log_scale=(False, log_scale))
+    sns.histplot(data[column], bins=1000, kde=True,
+                 log_scale=(False, log_scale))
     plt.title(text + ' Distribution', fontsize=16)
     plt.xlabel(text, fontsize=14)
     y_label = 'Frequency (Log Scale)' if log_scale else 'Frequency'
@@ -46,6 +51,7 @@ def plot_score_distribution(data, column="score", text="Fitness Score", log_scal
         plt.ion()
         plt.show()
 
+
 def normalized_score(data, column="score"):
     # Calculate the minimum and maximum values of the score column
     min_score = data[column].min()
@@ -54,10 +60,12 @@ def normalized_score(data, column="score"):
     # Apply Min-Max Normalization
     return (data[column] - min_score) / (max_score - min_score)
 
+
 def plot_normalized_score_distribution(data, column="normalized_score", text="Fitness Score", log_scale=False, path=None):
     sns.set(style="whitegrid")
     plt.figure(figsize=(10, 6))
-    sns.histplot(data[column], bins=1000, kde=True, log_scale=(False, log_scale))
+    sns.histplot(data[column], bins=1000, kde=True,
+                 log_scale=(False, log_scale))
     plt.title('Normalized ' + text + ' Distribution', fontsize=16)
     plt.xlabel('Normalized ' + text, fontsize=14)
     # Set the y-axis label based on whether log scale is used
@@ -83,7 +91,7 @@ def plot_sequence_length_distribution(data, path=None):
     plt.tight_layout()
     if path is not None:
         plt.savefig(path, format='png')
-    else: 
+    else:
         plt.ion()
         plt.show()
 
@@ -111,35 +119,41 @@ def plot_mutations_number(data, column='number_of_mutations', annotation=False, 
         plt.show()
 
 
-
 def parse_fasta(fasta_file, log=False):
     with open(fasta_file, 'r') as file:
         sequence_id = ''
         sequence = ''
         for line in file:
             if line.startswith('>'):
-                sequence_id = line[1:].strip()  # Removes the '>' and any trailing newline characters
+                # Removes the '>' and any trailing newline characters
+                sequence_id = line[1:].strip()
             else:
                 sequence += line.strip()  # Adds the sequence line, removing any trailing newlines
         if log:
             print("Sequence ID:", sequence_id)
             print("Sequence:", sequence)
         return sequence_id, sequence
-    
+
+
 def plot_mutations_heatmap(mutation_counts, zoom_region=None, path=None):
     sns.set(style="white")
     mutation_df = pd.DataFrame(mutation_counts)
-    fig = plt.figure() 
-    fig, (ax_main, ax_zoom) = plt.subplots(1, 2, figsize=(20, 10), gridspec_kw={'width_ratios': [3, 1]})
-    sns.heatmap(np.transpose(mutation_df), cmap='viridis', cbar=True, ax=ax_main)
-    ax_main.set_title('Mutation Heatmap per Amino Acid and Position', fontsize=16)
+    fig = plt.figure()
+    fig, (ax_main, ax_zoom) = plt.subplots(1, 2, figsize=(
+        20, 10), gridspec_kw={'width_ratios': [3, 1]})
+    sns.heatmap(np.transpose(mutation_df),
+                cmap='viridis', cbar=True, ax=ax_main)
+    ax_main.set_title(
+        'Mutation Heatmap per Amino Acid and Position', fontsize=16)
     ax_main.set_xlabel('Position in Sequence', fontsize=14)
     ax_main.set_ylabel('Amino Acids', fontsize=14)
 
     if zoom_region is not None:
         start, end = zoom_region
-        sns.heatmap(np.transpose(mutation_df.iloc[start:end, :]), ax=ax_zoom, cmap='viridis', cbar=True)
-        ax_zoom.set_title(f'Zoomed Region: Positions {start} to {end}', fontsize=16)
+        sns.heatmap(np.transpose(
+            mutation_df.iloc[start:end, :]), ax=ax_zoom, cmap='viridis', cbar=True)
+        ax_zoom.set_title(
+            f'Zoomed Region: Positions {start} to {end}', fontsize=16)
         ax_zoom.set_xlabel('Position in Sequence', fontsize=14)
         ax_zoom.set_ylabel('Amino Acids', fontsize=14)
     else:
@@ -152,15 +166,17 @@ def plot_mutations_heatmap(mutation_counts, zoom_region=None, path=None):
         plt.ion()
         plt.show()
 
+
 def PCA_2d(data_type, model, layers, reduction, output_path='default', labels_col='score', labeling='continuous', custom_data=None):
     if output_path == 'default':
         output_path = f'./plmfit/data/{data_type}/embeddings/plots'
     else:
         output_path = output_path
-    
-    data = utils.load_dataset(data_type) if custom_data is None else custom_data
 
-    if labeling=='discrete':
+    data = utils.load_dataset(
+        data_type) if custom_data is None else custom_data
+
+    if labeling == 'discrete':
         # Mapping species to colors for the filtered dataset
         labels_unique = data[labels_col].unique()
         labels_to_int = {labels: i for i, labels in enumerate(labels_unique)}
@@ -175,7 +191,7 @@ def PCA_2d(data_type, model, layers, reduction, output_path='default', labels_co
         scaler = MinMaxScaler()
         scores_scaled = scaler.fit_transform(scores.reshape(-1, 1)).flatten()
         c = scores_scaled
-        cmap='viridis'
+        cmap = 'viridis'
 
     for layer in layers:
         # Load embeddings
@@ -210,3 +226,116 @@ def PCA_2d(data_type, model, layers, reduction, output_path='default', labels_co
 
         plt.close()
 
+
+def create_loss_plot(training_losses, validation_losses):
+    fig = plt.figure(figsize=(10, 5))
+    plt.plot(training_losses, label='Training Loss')
+    plt.plot(validation_losses, label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Loss')
+    plt.legend()
+    return fig
+
+
+def plot_roc_curve(y_test_list, y_pred_list):
+    fpr, tpr, thresholds = roc_curve(y_test_list, y_pred_list)
+    roc_auc_val = auc(fpr, tpr)
+
+    fig = plt.figure(figsize=(10, 5))
+    plt.plot(fpr, tpr, color='darkorange', lw=2,
+             label='ROC curve (area = %0.2f)' % roc_auc_val)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    return fig
+
+
+def plot_actual_vs_predicted(y_test_list, y_pred_list):
+    fig = plt.figure(figsize=(10, 5))
+    plt.scatter(y_test_list, y_pred_list, color='darkorange', edgecolors='k', label='Predicted vs Actual')
+    plt.plot([y_test_list.min(), y_test_list.max()], [y_test_list.min(), y_test_list.max()], 'k--', lw=2, label='Ideal')
+    plt.xlabel('Actual')
+    plt.ylabel('Predicted')
+    plt.title('Actual vs. Predicted')
+    plt.legend()
+    return fig
+
+
+def binary_accuracy(y_true, y_pred):
+    y_pred_tag = torch.round(y_pred)
+    correct_results_sum = (y_pred_tag == y_true).sum().float()
+    acc = correct_results_sum/y_true.shape[0]
+    acc = torch.round(acc * 100)
+    return acc
+
+
+def evaluate_classification(model, dataloaders_dict, device):
+    model.eval()  # Set the model to evaluation mode
+    y_pred_list = []
+    y_test_list = []
+    for inputs, labels in dataloaders_dict['test']:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs).squeeze()
+        y_pred = outputs
+        y_pred_list.extend(y_pred.detach().cpu().numpy())
+        y_test_list.extend(labels.detach().cpu().numpy())
+
+    # Calculate metrics
+    acc = binary_accuracy(torch.tensor(y_test_list), torch.tensor(y_pred_list))
+    roc_auc = roc_auc_score(y_test_list, y_pred_list)
+    mcc = matthews_corrcoef(y_test_list, np.round(y_pred_list))
+    cm = confusion_matrix(y_test_list, np.round(y_pred_list)).tolist()
+
+    # Assuming cm is your confusion matrix as a numpy array
+    cm_dict = {
+        "True Negatives (TN)": int(cm[0][0]),
+        "False Positives (FP)": int(cm[0][1]),
+        "False Negatives (FN)": int(cm[1][0]),
+        "True Positives (TP)": int(cm[1][1])
+    }
+
+    # Log evaluation metrics
+    eval_metrics = {
+        "Final Accuracy (%)": acc.item(),
+        "ROC AUC": roc_auc,
+        "MCC": mcc,
+        "Confusion Matrix": cm_dict
+    }
+
+    fig = plot_roc_curve(y_test_list, y_pred_list)
+
+    return eval_metrics, fig
+
+
+def evaluate_regression(model, dataloaders_dict, device):
+    model.eval()  # Set the model to evaluation mode
+    y_pred_list = []
+    y_test_list = []
+    with torch.no_grad():
+        for inputs, labels in dataloaders_dict['test']:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs).squeeze()
+            y_pred_list.extend(outputs.detach().cpu().numpy())
+            y_test_list.extend(labels.detach().cpu().numpy())
+
+    # Calculate metrics
+    mse = mean_squared_error(y_test_list, y_pred_list)
+    rmse = np.sqrt(mse)
+    mae = mean_absolute_error(y_test_list, y_pred_list)
+    r2 = r2_score(y_test_list, y_pred_list)
+
+    eval_metrics = {
+        "MSE": float(mse),  # Convert to Python float
+        "RMSE": float(rmse),  # Convert to Python float
+        "MAE": float(mae),  # Convert to Python float
+        "R^2": float(r2)  # Convert to Python float
+    }
+
+    fig = plot_actual_vs_predicted(y_test_list, y_pred_list)
+
+    return eval_metrics, fig
