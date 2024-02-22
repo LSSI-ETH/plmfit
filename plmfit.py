fef0ffb8e31ec15435fa405c23c60ffabf08fedd
bikias.thomas@gmail.com <bikias.thomas@gmai.com>
2024-02-22 12:13:02 +0100

    added classification and regression heads

--- plmfit.py
+++ plmfit.py
@@ -1,30 +1,9 @@
-import os
-import math
-import pandas as pd
-from plmfit.language_models.progen2.models.progen.modeling_progen import ProGenForCausalLM
-from tokenizers import Tokenizer
 import torch
-import time
-from scipy.stats.stats import pearsonr
-import json
-import matplotlib.pyplot as plt
-from transformers import AutoTokenizer, EsmForSequenceClassification
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-import torch.nn as nn
-import torch.utils.data as data_utils
-from torch.nn import init
-import numpy as np
-from scipy import stats
-from sklearn.preprocessing import StandardScaler
-from torch.utils.data import DataLoader
 import plmfit.logger as l
 import argparse
-import plmfit.shared_utils.utils as utils
 from plmfit.models.pretrained_models import *
-
-import torchmetrics
+import plmfit.shared_utils.utils as utils
+import plmfit.models.downstream_heads as heads
 
 
 parser = argparse.ArgumentParser(description='plmfit_args')
@@ -34,7 +13,8 @@ parser.add_argument('--ft_method', type=str, default='feature_extraction')
 parser.add_argument('--data_type', type=str, default='aav')
 # here you specifcy the different splits
 parser.add_argument('--data_file_name', type=str, default='data_train')
-parser.add_argument('--embs', type=str, default='aav_progen2-small_embs_layer12_mean')
+parser.add_argument('--embs', type=str,
+                    default='aav_progen2-small_embs_layer12_mean')
 
 # option ['mlp' , 'cnn' , 'inception', '{a custome head}' , 'attention']
 parser.add_argument('--head', type=str, default='linear')
@@ -52,24 +32,23 @@ parser.add_argument('--batch_size', type=int, default=5)
 parser.add_argument('--epochs', type=int, default=5)
 parser.add_argument('--val_split', type=float, default=0.2)
 parser.add_argument('--lr', type=float, default=0.0001)
+parser.add_argument('--weight_decay', type=float, default=0)
+parser.add_argument('--scaler', type=str, default=None)
 parser.add_argument('--optimizer', type=str, default='adam')
+parser.add_argument('--loss_f', type=str, default='mse')
 parser.add_argument('--function', type=str, default='extract_embeddings')
-parser.add_argument('--reduction', type=str, default='mean')
-parser.add_argument('--layer', type=str, default=0)
+parser.add_argument('--reduction', type=str, default='mean', help='Reduction technique')
+parser.add_argument('--layer', type=str, default=0, help='PLM layer to be used')
 
-parser.add_argument('--output_dir', type=str, default='default')
+parser.add_argument('--output_dir', type=str, default='default', help='Output directory for created files')
 
 args = parser.parse_args()
 
-# logger = l.Logger(
-#     f'logger_{args.ft_method}_{args.head}_{args.data_type}_SPECS:_filename:{args.data_file_name}_gpus:{args.gpus}_gres:{args.gres}_nodes:{args.nodes}')
 
-if __name__ == '__main__':
-
-    # Preparing input token (embeddings, one hot encoded or categorical encoded)
+def init_plm(model_name):
     model = None
     supported_progen2 = ['progen2-small', 'progen2-medium', 'progen2-xlarge']
-    supported_ESM = ["esm2_t6_8M_UR50D", "esm2_t12_35M_UR50D", 
+    supported_ESM = ["esm2_t6_8M_UR50D", "esm2_t12_35M_UR50D",
                      "esm2_t30_150M_UR50D", "esm2_t33_650M_UR50D"]
     supported_Ankh = ['ankh-base', 'ankh-large', 'ankh2-large']
     
@@ -90,13 +69,73 @@ if __name__ == '__main__':
     else: 
         raise 'PLM not supported'
 
-    assert model != None , 'Model is not initialized'
+    return model
+
+
+if __name__ == '__main__':
 
     if args.function == 'extract_embeddings':
+        model = init_plm(args.plm)
+        assert model != None, 'Model is not initialized'
 
-        model.extract_embeddings(data_type = args.data_type , layer= args.layer , reduction= args.reduction, output_dir=args.output_dir)
+        model.extract_embeddings(data_type=args.data_type, layer=args.layer,
+                                 reduction=args.reduction, output_dir=args.output_dir)
+
     elif args.function == 'fine_tuning':
-        model.f
+        if args.ft_method == 'feature_extraction':
+
+            config = utils.load_head_config(args.head_config)
+            if config['network_type'] != args.head:
+                raise f'Wrong configuration file for "{args.head}" head'
+
+            # Load dataset
+            data = utils.load_dataset(args.data_type)
+
+            # Load embeddings and scores
+            embeddings = utils.load_embeddings(
+                data_type=args.data_type, model=args.plm, layer=args.layer, reduction=args.reduction)
+
+            if args.head == 'logistic_regression':
+                binary_scores = data['binary_score'].values
+                binary_scores = torch.tensor(
+                    binary_scores, dtype=torch.float32)
+
+                data_loaders = utils.create_data_loaders(
+                    embeddings, binary_scores, scaler=args.scaler, batch_size=args.batch_size)
+
+                pred_model = heads.LogisticRegression(config)
+
+                output_path = f'./plmfit/data/{args.data_type}/models/classification/{args.plm}_{args.layer}_{args.reduction}'
+                logger = l.Logger('classification', output_path)
+                logger.save_data(vars(args), 'Arguments')
+                logger.save_data(config, 'Head config')
+
+                fine_tuner = FullRetrainFineTuner(
+                    epochs=args.epochs, lr=args.lr, weight_decay=args.weight_decay, batch_size=args.batch_size, val_split=0.2, optimizer=args.optimizer, loss_function=args.loss_f, log_interval=-1, task_type='classification')
+                fine_tuner.train(
+                    pred_model, dataloaders_dict=data_loaders, logger=logger)
+            elif args.head == 'linear_regression':
+                scores = data['score'].values
+                scores = torch.tensor(
+                    scores, dtype=torch.float32)
+
+                data_loaders = utils.create_data_loaders(
+                    embeddings, scores, scaler=args.scaler, batch_size=args.batch_size)
+                
+                pred_model = heads.LinearRegression(config)
+                output_path = f'./plmfit/data/{args.data_type}/models/regression/{args.plm}_{args.layer}_{args.reduction}'
+                logger = l.Logger('regression', output_path)
+                logger.save_data(vars(args), 'Arguments')
+                logger.save_data(config, 'Head config')
+                fine_tuner = FullRetrainFineTuner(
+                    epochs=args.epochs, lr=args.lr, weight_decay=args.weight_decay, batch_size=args.batch_size, val_split=0.2, optimizer=args.optimizer, loss_function=args.loss_f, log_interval=-1, task_type='regression')
+                fine_tuner.train(
+                    pred_model, dataloaders_dict=data_loaders, logger=logger)
+
+            else:
+                raise ValueError('Head type not supported')
+        else:
+            raise ValueError('Fine Tuning method not supported')
     else:
 
-        raise 'Function is not supported'
+        raise ValueError('Function is not supported')
