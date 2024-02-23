d3d846cd54ae0802d723786b2dbdcaddabb53808
bikias.thomas@gmail.com <bikias.thomas@gmai.com>
2024-02-23 10:47:47 +0100

    New PCA graphs

--- scripts/visualize_embeddings.py
+++ scripts/visualize_embeddings.py
@@ -8,19 +8,19 @@ import pandas as pd
 import plmfit.shared_utils.utils as utils
 
 layers = ['last']
-data_type = 'meltome'
-model = 'progen2-small'
-reduction = 'mean'
+data_type = 'aav'
+model = 'progen2-xlarge'
+reduction = 'bos'
 
 # Define human-related species to be consolidated
-human_species = ['HepG2', 'HAOEC', 'colon_cancer', 'HL60', 'HEK293T',
-                 'U937', 'Jurkat', 'HaCaT', 'K562', 'pTcells']
+# human_species = ['HepG2', 'HAOEC', 'colon_cancer', 'HL60', 'HEK293T',
+#                  'U937', 'Jurkat', 'HaCaT', 'K562', 'pTcells']
 
 data = utils.load_dataset(data_type=data_type)
 # Extract species from the identifier and consolidate human-related species
-data['species'] = data['id'].apply(
-    lambda x: 'Homo_sapiens' if any(human in x for human in human_species) else '_'.join(
-        x.split('_')[1:3]) if len(x.split('_')) > 3 else '_'.join(x.split('_')[1:])
-)
+# data['species'] = data['id'].apply(
+#     lambda x: 'Homo_sapiens' if any(human in x for human in human_species) else '_'.join(
+#         x.split('_')[1:3]) if len(x.split('_')) > 3 else '_'.join(x.split('_')[1:])
+# )
 
-de.PCA_2d(data_type=data_type, model=model, layers=layers, reduction=reduction, labels_col='species', labeling='discrete', custom_data=data)
+de.PCA_2d(data_type=data_type, model=model, layers=layers, reduction=reduction, labels_col='binary_score', labeling='discrete')
