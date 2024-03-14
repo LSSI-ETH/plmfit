cc162e0996afecebfb3c97a36a8ae373d9b43590
championsnet <vaggelisstam@gmail.com>
2024-03-14 17:02:47 +0100

    LoRA mass bash scripts

---
+++ scripts/lora/lora_mass.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --tasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=24:00:00
+
+module load eth_proxy
+module load gcc/8.2.0  python_gpu/3.11.2
+
+nvcc --version
+nvidia-smi
+python3 plmfit.py --function $1 --ft_method $2 --head_config $3 \
+        --data_type $4 --plm $5 --layer $6 --reduction $7 \
+        --output_dir ${8} --experiment_dir ${9} --experiment_name ${10}
---
+++ scripts/lora/submit_lora_mass.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Path to the CSV file
+csv_file="./scripts/lora/experiments_setup.csv"
+
+uid=$(date +%Y%m%d_%H%M%S)
+
+# Skip the header line
+tail -n +2 "$csv_file" | while IFS=$'\t' read -r function ft_method data_type plm head task head_config layer reduction output_dir gpus gres mem_per_cpu
+do
+  output_dir="$output_dir"
+  experiment_name="${data_type}_${plm}_${ft_method}_${layer}_${reduction}_${head}_${task}"
+  experiment_dir="$output_dir/$function/$experiment_name"
+  sbatch --job-name="lora_${uid}" \
+         --output="$experiment_dir/euler_output.out" \
+         --error="$experiment_dir/euler_error.err" \
+         --mem-per-cpu="$mem_per_cpu" \
+         --gpus="$gpus"  \
+         --gpus-per-node="$gpus" \
+         --gres="gpumem:$gres" \
+         scripts/lora/lora_mass.sh \
+         "$function" "$ft_method" "$head_config" "$data_type" "$plm" "$layer" "$reduction" "$output_dir" "$experiment_dir" "$experiment_name"
+done
