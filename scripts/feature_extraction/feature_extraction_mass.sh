c397fbc898ba99e9dd5b853a6a815cf6fb649cef
bikias.thomas@gmail.com <bikias.thomas@gmai.com>
2024-03-22 15:41:02 +0100

    Adjust for special treatment

--- scripts/feature_extraction/feature_extraction_mass.sh
+++ scripts/feature_extraction/feature_extraction_mass.sh
@@ -3,13 +3,13 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --tasks-per-node=1
-#SBATCH --time=4:00:00
+#SBATCH --time=8:00:00
 
 module load eth_proxy
 module load gcc/8.2.0  python_gpu/3.11.2
 
 nvcc --version
 nvidia-smi
-python3 plmfit.py --function $1 --ft_method $2 --head_config $3 \
-        --data_type $4 --plm $5 --layer $6 --reduction $7 \
-        --output_dir ${8} --experiment_dir ${9} --experiment_name ${10}
+python3 plmfit.py --function $1 --ft_method $2 --head_config $3 --ray_tuning $4 \
+        --data_type $5 --plm $6 --layer $7 --reduction $8 \
+        --output_dir ${9} --experiment_dir ${10} --experiment_name ${11}
