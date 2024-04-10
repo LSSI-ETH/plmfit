ec36de9908c28e2fd43b94a26111b1b05eab9abb
championsnet <vaggelisstam@gmail.com>
2024-04-10 11:30:07 +0200

    Euler scripts

--- scripts/lora/lora_mass.sh
+++ scripts/lora/lora_mass.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --tasks-per-node=1
-#SBATCH --time=15:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+
+export DATA_DIR='/cluster/home/estamkopoulo/plmfit_workspace/plmfit/plmfit'
+export NCCL_DEBUG=WARN
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+export PYTHONFAULTHANDLER=1
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+# Export the global rank using SLURM_PROCID
+export RANK=$SLURM_PROCID
+echo "MASTER_ADDR:MASTER_PORT="${MASTER_ADDR}:${MASTER_PORT}
 
 module load eth_proxy
 module load gcc/8.2.0  python_gpu/3.11.2
+module load cuda/12.1.1
 
 nvcc --version
 nvidia-smi
-python3 plmfit.py --function $1 --ft_method $2 --head_config $3 \
+watch -n 10 nvidia-smi --query-gpu=timestamp,name,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv >> ${9}/gpu_usage.log 2>&1 &
+srun python3 plmfit.py --function $1 --ft_method $2 --head_config $3 \
         --data_type $4 --plm $5 --layer $6 --reduction $7 \
-        --output_dir ${8} --experiment_dir ${9} --experiment_name ${10}
+        --output_dir ${8} --experiment_dir ${9} --experiment_name ${10} --gpus ${11} --nodes ${12} --beta True
+kill $(pgrep watch)
--- scripts/lora/submit_lora_mass.sh
+++ scripts/lora/submit_lora_mass.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
+select_gpus() {
+  bash ./scripts/find_configuration.sh "$gres" "$gpus" "$mem_per_cpu"
+}
+
 # Path to the CSV file
 csv_file="./scripts/lora/experiments_setup.csv"
 
 uid=$(date +%Y%m%d_%H%M%S)
 
 # Skip the header line
-tail -n +2 "$csv_file" | while IFS=$'\t' read -r function ft_method data_type plm head task head_config layer reduction output_dir gpus gres mem_per_cpu
+tail -n +2 "$csv_file" | while IFS=$'\t' read -r function ft_method data_type plm head task head_config layer reduction output_dir gpus gres mem_per_cpu nodes
 do
+  # read -r node_name gres gpus <<< "$(select_gpus)"
+  # echo "$node_name $gres $gpus"
   output_dir="$output_dir"
   experiment_name="${data_type}_${plm}_${ft_method}_${layer}_${reduction}_${head}_${task}"
   experiment_dir="$output_dir/$function/$ft_method/$experiment_name/$uid"
+  total_gpus="$((${gpus}*${nodes}))"
+  
   sbatch --job-name="lora_${uid}" \
          --output="$experiment_dir/euler_output.out" \
          --error="$experiment_dir/euler_error.err" \
          --mem-per-cpu="$mem_per_cpu" \
-         --gpus="$gpus"  \
-         --gpus-per-node="$gpus" \
-         --gres="gpumem:$gres" \
+         --nodes=$nodes \
+         --ntasks=$total_gpus \
+         --ntasks-per-node=$gpus \
+         --gpus-per-node=$gres:$gpus \
          scripts/lora/lora_mass.sh \
-         "$function" "$ft_method" "$head_config" "$data_type" "$plm" "$layer" "$reduction" "$output_dir" "$experiment_dir" "$experiment_name"
+         "$function" "$ft_method" "$head_config" "$data_type" "$plm" "$layer" "$reduction" "$output_dir" "$experiment_dir" "$experiment_name" "$gpus" "$nodes"
 done
