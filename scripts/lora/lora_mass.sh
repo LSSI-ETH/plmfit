26521ae060cb3cd9333e0f2086dfe6a29d074f2b
championsnet <vaggelisstam@gmail.com>
2024-04-16 18:40:20 +0200

    Better gpu monitoring

--- scripts/lora/lora_mass.sh
+++ scripts/lora/lora_mass.sh
@@ -19,8 +19,16 @@ module load cuda/12.1.1
 
 nvcc --version
 nvidia-smi
-watch -n 10 nvidia-smi
+nvidia-smi --query-gpu=timestamp,name,utilization.gpu,memory.total,memory.used --format=csv -l 100 > ${9}/gpu_usage.log 2>&1 &
+# Store the PID of the nvidia-smi background process
+NVIDIA_SMI_PID=$!
+
+# Start logging CPU RAM usage
+watch -n 100 "free -m | sed -r 's/\x1B(\[[0-9;]*[mGKHJ]|\\?\[[0-9]{1,2}h|\\?\[[0-9]{1,2}l)//g'" > ${9}/cpu_usage.log 2>&1 &
+CPU_FREE_PID=$!
+
 srun python3 plmfit.py --function $1 --ft_method $2 --head_config $3 \
         --data_type $4 --plm $5 --layer $6 --reduction $7 \
         --output_dir ${8} --experiment_dir ${9} --experiment_name ${10} --gpus ${11} --nodes ${12} --beta True
-kill $(pgrep watch)
+kill $NVIDIA_SMI_PID
+kill $CPU_FREE_PID
