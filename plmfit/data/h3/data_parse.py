8e56b95ebd8c9a2e43a57e3c5caa050b8d88516a
championsnet <vaggelisstam@gmail.com>
2024-05-14 17:26:27 +0200

    Better data parsers (trivial)

--- plmfit/data/h3/data_parse.py
+++ plmfit/data/h3/data_parse.py
@@ -13,16 +13,14 @@ def read_fasta(fasta_path):
         })
     return sequences
 
-def filter_sequences(sequences, max_length=1000, min_length=0):
+def filter_sequences(sequences, max_length=1000):
     """
     Filter sequences to remove those containing '*' and those longer than max_length.
     """
     filtered_data = pd.DataFrame(sequences)
     filtered_data["len"] = filtered_data["aa_seq"].apply(len)  # Calculate sequence length
     filtered_data = filtered_data[~filtered_data["aa_seq"].str.contains("\*")]  # Remove sequences containing '*'
-    filtered_data = filtered_data[~filtered_data["aa_seq"].str.contains("X")]  # Remove sequences containing 'X'
     filtered_data = filtered_data[filtered_data["len"] <= max_length]  # Limit sequences to max_length
-    filtered_data = filtered_data[filtered_data["len"] >= min_length]  # Limit sequences to min_length
     return filtered_data
 
 def remove_duplicates(data):
@@ -34,19 +32,19 @@ def remove_duplicates(data):
 
 def main():
     script_dir = os.path.dirname(__file__)  # Absolute dir the script is in
-    fasta_path = "H1_sequences.fasta"  # Path to your FASTA file
+    fasta_path = "H3_sequences.fasta"  # Path to your FASTA file
 
     # Read the dataset from the FASTA file
     sequences = read_fasta(os.path.join(script_dir, fasta_path))
 
     # Filter sequences and prepare DataFrame
-    filtered_data = filter_sequences(sequences, max_length=566, min_length=566)
+    filtered_data = filter_sequences(sequences)
 
     # Remove duplicate sequences
     unique_data = remove_duplicates(filtered_data)
 
     # Save the filtered and unique data to a CSV file
-    csv_output_path = os.path.join(script_dir, "h1_data_full.csv")
+    csv_output_path = os.path.join(script_dir, "h3_data_full.csv")
     unique_data.to_csv(csv_output_path, index=False)
     print(f"Processed data saved to {csv_output_path}")
 
