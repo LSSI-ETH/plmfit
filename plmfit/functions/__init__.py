a480f770f1c95ba29b9386d81aefe924996235ea
championsnet <vaggelisstam@gmail.com>
2024-05-14 17:26:35 +0200

    Predict function first steps

--- plmfit/functions/__init__.py
+++ plmfit/functions/__init__.py
@@ -2,4 +2,4 @@ from plmfit.functions.extract_embeddings import extract_embeddings
 from plmfit.functions.fine_tune import fine_tune
 from plmfit.functions.onehot import onehot
 from plmfit.functions.feature_extraction import feature_extraction
-from plmfit.functions.developing import developing
+from plmfit.functions.predict import predict
--- plmfit/functions/developing.py
+++
@@ -1,8 +0,0 @@
-from plmfit.shared_utils import utils, data_explore
-
-def developing(args, logger):
-    # Adapters development
-    model = utils.init_plm(args.plm, logger)
-    assert model != None, 'Model is not initialized'
-
-    print(model)
