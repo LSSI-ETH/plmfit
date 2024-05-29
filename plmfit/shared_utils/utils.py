1045bc85d2fdc15628979da8f2d8b866cb9228b4
championsnet <vaggelisstam@gmail.com>
2024-05-29 10:43:28 +0200

    Creates validation set if split is defined but no validation found

--- plmfit/shared_utils/utils.py
+++ plmfit/shared_utils/utils.py
@@ -82,6 +82,12 @@ def create_data_loaders(dataset, scores, split=None, test_size=0.2, validation_s
         X_train, X_val, X_test = dataset[split == 'train'], dataset[split == 'validation'], dataset[split == 'test']
         y_train, y_val, y_test = scores[split == 'train'], scores[split == 'validation'], scores[split == 'test']
 
+        # Check if the validation set is empty and split the training data if necessary
+        if X_val.shape[0] == 0 or y_val.shape[0] == 0:
+            X_train, X_val, y_train, y_val = train_test_split(
+                X_train, y_train, test_size=validation_size, random_state=42
+            )
+
     # Scale the features if scaler is provided
     if scaler:
         scaler = StandardScaler()
