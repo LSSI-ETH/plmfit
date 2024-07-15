e56d7993d5f458da45a9629f7c71f9a0bbbc8810
danielleshlesinger <dshlesin@eu-login-43.euler.ethz.ch>
2024-07-15 13:09:56 +0200

    debug train

--- scripts/extract_embeddings/extract_embeddings_mass.sh
+++ scripts/extract_embeddings/extract_embeddings_mass.sh
@@ -4,15 +4,17 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --tasks-per-node=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=16:00:00
+#SBATCH --time=70:00:00
 echo "JOB ID: $SLURM_JOBID"
 
-export HF_HOME='/cluster/scratch/estamkopoulo/'
-export HF_HUB_CACHE='/cluster/scratch/estamkopoulo/'
+export HF_HOME='/cluster/scratch/dshlesin/'
+export HF_HUB_CACHE='/cluster/scratch/dshlesin/'
+
 module load eth_proxy
 module load stack/2024-06 gcc/12.2.0
 module load python/3.11.6 cuda/12.1.1 ninja/1.11.1
-
+source /cluster/home/dshlesin/envs/myenv_plm/bin/activate
+export DATA_DIR='/cluster/scratch/dshlesin/Manuscript/plmfit/plmfit'
 nvcc --version
 nvidia-smi
 nvidia-smi --query-gpu=timestamp,name,utilization.gpu,memory.total,memory.used --format=csv -l 1 > ${7}/gpu_usage.log 2>&1 &
