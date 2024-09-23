c9586a3b35eaa22bdeeab3af3b9d76ec0b406c15
Vaggelis Stamkopoulos <vaggelisstam@gmail.com>
2024-09-23 17:42:20 +0200

    Delete unused file

--- multi_gpu_inference.py
+++
@@ -1,6 +0,0 @@
-import torch
-from plmfit.language_models.progen2.models.progen.modeling_progen import ProGenForCausalLM
-
-print("loading model...")
-py_model = ProGenForCausalLM.from_pretrained(f'./plmfit/language_models/progen2/checkpoints/progen2-small')
-print(py_model)
