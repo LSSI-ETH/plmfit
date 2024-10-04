4643a068cc868d4e99fb4a986574c1a30ccecc84
championsnet <vaggelisstam@gmail.com>
2024-10-04 13:53:20 +0200

    setups from FE and OHE SLURM with lightning

--- scripts/feature_extraction/submit_feature_extraction_mass.sh
+++ scripts/feature_extraction/submit_feature_extraction_mass.sh
@@ -6,7 +6,7 @@ csv_file="./scripts/feature_extraction/experiments_setup.csv"
 uid=$(date +%Y%m%d_%H%M%S)
 
 # Skip the header line
-tail -n +2 "$csv_file" | while IFS=$'\t' read -r function ft_method data_type split plm head task head_config ray_tuning layer reduction output_dir gpus gres mem_per_cpu nodes run_time
+tail -n +2 "$csv_file" | while IFS=$'\t' read -r function ft_method data_type split plm head task head_config ray_tuning layer reduction output_dir gpus gres mem_per_cpu nodes run_time experimenting
 do
   output_dir="$output_dir"
   experiment_name="${data_type}_${split}_${plm}_${ft_method}_${layer}_${reduction}_${head}_${task}"
@@ -16,14 +16,11 @@ do
          --output="$experiment_dir/euler_output.out" \
          --error="$experiment_dir/euler_error.err" \
          --mem-per-cpu="$mem_per_cpu" \
-         --nodes="$nodes" \
-         --ntasks="$nodes" \
-         --cpus-per-task=1 \
-         --tasks-per-node=1 \
-         --gpus-per-node=1 \
-         --gres=gpumem:${gres} \
-         --time="${run_time}:00:00" \
-         --nice=1 \
+         --nodes=$nodes \
+         --ntasks=$total_gpus \
+         --ntasks-per-node=$gpus \
+         --gpus-per-node=$gres:$gpus \
+         --time=$run_time:00:00 \
          scripts/feature_extraction/feature_extraction_mass.sh \
-         "$function" "$ft_method" "$head_config" "$ray_tuning" "$data_type" "$split" "$plm" "$layer" "$reduction" "$output_dir" "$experiment_dir" "$experiment_name" "$gpus"
+         "$function" "$ft_method" "$head_config" "$ray_tuning" "$data_type" "$split" "$plm" "$layer" "$reduction" "$output_dir" "$experiment_dir" "$experiment_name" "$gpus" "$nodes" "$experimenting"
 done
--- scripts/one_hot/one_hot_mass.sh
+++ scripts/one_hot/one_hot_mass.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=ray_workload    # create a short name for your job
-#SBATCH --nodes=1          # node count
-#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --tasks-per-node=1
-#SBATCH --gpus-per-node=1
-#SBATCH --gres=gpumem:24g
-#SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
 
-
+set -a && source .env && set +a
 
 module load eth_proxy
 module load stack/2024-06 gcc/12.2.0
 module load python/3.11.6 cuda/12.1.1 ninja/1.11.1
-
-nvidia-smi
-nvidia-smi --query-gpu=timestamp,name,utilization.gpu,memory.total,memory.used --format=csv -l 1 > ${7}/gpu_usage.log 2>&1 &
-# Store the PID of the nvidia-smi background process
-NVIDIA_SMI_PID=$!
-
-set -a && source .env && set +a
-source $VIRTUAL_ENV/bin/activate
 
 export NCCL_DEBUG=WARN
 export NCCL_P2P_DISABLE=1
@@ -35,8 +20,24 @@ echo "MASTER_ADDR:MASTER_PORT="${MASTER_ADDR}:${MASTER_PORT}
 
 
 
-python3 -u plmfit --function $1 --head_config $2 \
+export CUDA_LAUNCH_BLOCKING=1
+source $VIRTUAL_ENV/bin/activate
+
+nvidia-smi
+nvidia-smi --query-gpu=timestamp,name,utilization.gpu,memory.total,memory.used --format=csv -l 1 > ${7}/gpu_usage.log 2>&1 &
+# Store the PID of the nvidia-smi background process
+NVIDIA_SMI_PID=$!
+
+while true; do
+  myjobs -j $SLURM_JOBID >> ${7}/task_monitor.log 2>&1
+  sleep 1
+done &
+CPU_FREE_PID=$!
+
+python3 plmfit --function $1 --head_config $2 \
         --data_type $4 --split $5 --ray_tuning $3 \
-        --output_dir ${6} --experiment_dir ${7} --experiment_name ${8}
+        --output_dir ${6} --experiment_dir ${7} --experiment_name ${8} \
+        --gpus ${9} --nodes ${10} --beta True --experimenting ${11}
 
 kill $NVIDIA_SMI_PID
+kill $CPU_FREE_PID
