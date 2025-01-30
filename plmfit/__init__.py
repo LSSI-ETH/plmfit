0a237c52e15967035d1de6cd158013b0bb8af0e0
Vaggelis Stamkopoulos <vaggelisstam@gmail.com>
2025-01-30 16:03:27 +0100

    Brain fog and bug fix

--- plmfit/__init__.py
+++ plmfit/__init__.py
@@ -1,3 +1,8 @@
 __version__ = "1.0.0"
 __author__ = "Thomas Bikias, Evangelos Stamkopoulos"
 __credits__ = "Argonne National Laboratory"
+
+import os
+
+# Find current file absolute path
+os.environ.get("PLMFIT_PATH", os.path.dirname(os.path.abspath(__file__)))
--- plmfit/language_models/progen2/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-
-# Find current file absolute path
-os.environ.get("PLMFIT_PATH", os.path.dirname(os.path.abspath(__file__)))
