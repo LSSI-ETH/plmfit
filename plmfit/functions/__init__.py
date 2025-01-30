d4b6952838c47817708c6b143741f4867107acef
Vaggelis Stamkopoulos <vaggelisstam@gmail.com>
2025-01-30 15:59:17 +0100

    Try to fix relative paths in plmfit

--- plmfit/functions/__init__.py
+++ plmfit/functions/__init__.py
@@ -5,3 +5,7 @@ from plmfit.functions.feature_extraction import feature_extraction
 from plmfit.functions.predict import predict
 from plmfit.functions.blosum62 import blosum
 from plmfit.functions.categorical_train import categorical_train
+import os
+
+# Find current file absolute path
+os.environ.get("PLMFIT_PATH", os.path.dirname(os.path.abspath(__file__)))
