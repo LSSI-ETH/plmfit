3f4ab1c23ba2165add452ff93405ae8ef395d2c9
Vaggelis Stamkopoulos <vaggelisstam@gmail.com>
2025-01-30 15:40:28 +0100

    New bug fix for manifest

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
 from setuptools import find_packages, setup
 
-
 def read_requirements(filename: os.PathLike):
     with open(filename) as f:
         return [line.strip() for line in f if line.strip() and not line.startswith('#')]
@@ -9,6 +8,21 @@ def read_requirements(filename: os.PathLike):
 
 required_deps = read_requirements("requirements.txt")
 
+
+def __createManifest__(subdirs):
+    """inventory all files in path and create a manifest file"""
+    current = os.path.dirname(__file__)
+    relative_paths = [os.path.relpath(path, current) for path in subdirs]
+    with open(os.path.join(current, "MANIFEST.in"), "w") as manifest:
+        manifest.writelines(
+            "recursive-include {} *.json".format(" ".join(relative_paths))
+        )
+
+
+add_il = os.path.join(os.path.dirname(__file__), "plmfit")
+
+__createManifest__([add_il])
+print("MANIFEST.in created")
 setup(
     name="plmfit",
     version="1.0.0",
@@ -20,9 +34,5 @@ setup(
     zip_safe=True,
     install_requires=required_deps,
     include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'plmfit=plmfit.__main__:main'
-        ]
-    }
+    entry_points={"console_scripts": ["plmfit=plmfit.__main__:main"]},
 )
