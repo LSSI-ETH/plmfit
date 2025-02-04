26334a44e96abd3e1e0f4b3a12c96d8ee6bb62ef
championsnet <vaggelisstam@gmail.com>
2025-02-04 14:41:48 +0100

    New configs

--- scripts/feature_extraction/submit_feature_extraction_mass.sh
+++ scripts/feature_extraction/submit_feature_extraction_mass.sh
@@ -3,11 +3,11 @@
 # Path to the CSV file
 csv_file="./scripts/feature_extraction/experiments_setup.csv"
 
-uid=$(date +%Y%m%d_%H%M%S)
 
 # Skip the header line
 tail -n +2 "$csv_file" | while IFS=$'\t' read -r function data_type split plm head task head_config ray_tuning layer reduction output_dir gpus gres mem_per_cpu nodes run_time experimenting
 do
+  uid=$(date +%Y%m%d_%H%M%S)_$RANDOM
   output_dir="$output_dir"
   experiment_name="${data_type}_${split}_${plm}_${function}_${layer}_${reduction}_${head}_${task}"
   experiment_dir="$output_dir/$function/$experiment_name/$uid"
