33b711e70eb9a083a0bc2ffafd4eed02bde8b9c2
championsnet <vaggelisstam@gmail.com>
2025-02-19 08:57:53 +0100

    Configs and qol fixes on slurm scripts

--- scripts/train/submit_train_mass.sh
+++ scripts/train/submit_train_mass.sh
@@ -3,11 +3,11 @@
 # Path to the CSV file
 csv_file="./scripts/train/experiments_setup.csv"
 
-uid=$(date +%Y%m%d_%H%M%S)
 
 # Skip the header line
 tail -n +2 "$csv_file" | while IFS=$'\t' read -r function data_type split head task head_config ray_tuning output_dir gpus gres mem_per_cpu nodes run_time experimenting
 do
+  uid=$(date +%Y%m%d_%H%M%S)_$RANDOM
   output_dir="$output_dir"
   experiment_name="${data_type}_${split}_${head}_${task}"
   experiment_dir="$output_dir/$function/$experiment_name/$uid"
