60a5d55d6b9548c8917279dcee29bb8c00bf5432
Vaggelis Stamkopoulos <vaggelisstam@gmail.com>
2025-03-21 16:05:47 +0100

    Another bug fix

--- plmfit/shared_utils/utils.py
+++ plmfit/shared_utils/utils.py
@@ -1360,9 +1360,9 @@ def data_pipeline(dataset, split=None, weights=None, sampler=None, dev=False):
     split = (
         None if split == "sampled" and "sampled" not in dataset else dataset.get(split)
     )
-    
+
     if split is not None:
-        dataset_test = dataset[dataset[split] == "test"].reset_index(drop=True)
+        dataset_test = dataset[split == "test"].reset_index(drop=True)
 
     # If weights are provided, load them
     weights = None if weights is None else dataset.get(weights)
