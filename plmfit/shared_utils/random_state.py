f6523910a38e2f6a00b00f319ae739bc0d13df3b
Vaggelis Stamkopoulos <67751397+championsnet@users.noreply.github.com>
2025-05-12 16:33:44 +0200

    Update random_state.py

--- plmfit/shared_utils/random_state.py
+++ plmfit/shared_utils/random_state.py
@@ -1,29 +1,19 @@
 # plmfit/shared_utils/random_state.py
 import numpy as np
 import torch
-import random
 
-# Initialize the global random state
-seed = 42
-GLOBAL_RANDOM_STATE = np.random.RandomState(seed=seed)
-
-def get_random_state():
-    return GLOBAL_RANDOM_STATE
+# Global variable for numpy
+GLOBAL_RANDOM_STATE = None
 
 def set_seed(seed):
-
     global GLOBAL_RANDOM_STATE
-    GLOBAL_RANDOM_STATE = torch.Generator()
-    GLOBAL_RANDOM_STATE.manual_seed(seed)
+    GLOBAL_RANDOM_STATE = np.random.RandomState(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
-    np.random.seed(seed)
     random.seed(seed)
 
-def get_numpy_random_state():
-    np.random.RandomState(seed=seed)
+def get_random_state():
+    return GLOBAL_RANDOM_STATE
